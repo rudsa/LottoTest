a61c2fcae7c3c0286ad9fbbef4ea23bdcd920774
rudsa <rudsa@naver.com>
2021-11-18 19:04:44 +0900

    2021-11-18-3

--- src/LottoResultChecker.java
+++ src/LottoResultChecker.java
@@ -1,16 +1,19 @@
 public class LottoResultChecker {
     LottoNumGenerator lottoNumGenerator = new LottoNumGenerator();
-//    int[] lottoNumArray = lottoNumGenerator.getLottoNumArray();
-//    int[] lottoGameNumArray = lottoNumGenerator.getLottoGameNumArray();
-//    int[] lottoResultNumArray = lottoNumGenerator.getLottoGameResultArray();
-//
-//    LottoTest lottoTest = new LottoTest();
-//
-//
-//    public void lottoNumArray(){
-//        for(int i=0; i<lottoNumArray.length; i++){
-//
-//        }
-//    }
+    int [][] lottoGameNumArray = lottoNumGenerator.getLottoGameNumArray();
+    int [] lottoGameResultArray = lottoNumGenerator.getLottoGameResultArray();
+    int []lottoResult = new int [lottoGameNumArray.length];
+    int resultCnt = 0;
 
+    public int[] getLottoResult() {
+        return lottoResult;
+    }
+
+    public void lottoResultChecker(){
+        for(int i=0; i<lottoGameNumArray.length; i++){
+            for(int j=0; j<lottoGameNumArray[i].length; j++){
+
+            }
+        }
+    }
 }
--- src/LottoTest.java
+++ src/LottoTest.java
@@ -4,17 +4,12 @@ import java.util.Scanner;
 public class LottoTest {
     public static void main(String[] args) {
         LottoNumGenerator lottoNumGenerator = new LottoNumGenerator();
+        LottoResultChecker lottoResultChecker = new LottoResultChecker();
 
         Scanner scan = new Scanner(System.in);
         System.out.print("몇 게임 구매 하시겠어요?");
         int lottoGameBuyCnt = scan.nextInt();
         lottoNumGenerator.setLottoGameBuyCount(lottoGameBuyCnt);
-        int [][] test = lottoNumGenerator.getLottoGameNumArray();
-        for(int i=0; i<test.length; i++){
-            for(int j=0; j<test[i].length; j++){
-                System.out.print(test[i][j] + " ");
-            }
-            System.out.println();
-        }
+
     }
 }

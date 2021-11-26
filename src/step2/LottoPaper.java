3bae5b4ff855f17d01c4eb0bc571ac612eb7a6de
rudsa <rudsa@naver.com>
2021-11-26 23:30:54 +0900

    2021-11-26

--- src/step2/LottoPaper.java
+++ src/step2/LottoPaper.java
@@ -1,4 +1,6 @@
 package step2;
 
 public class LottoPaper {
+    public final int height = 8;
+    public final int width = 18;
 }
--- src/step2/LottoPen.java
+++ src/step2/LottoPen.java
@@ -1,4 +1,6 @@
 package step2;
 
 public class LottoPen {
+    public final int height = 15;
+    public final double width = 0.7;
 }

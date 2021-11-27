5d69655e678c7ef3b0560bdcfdf7404ac25190a3
rudsa <rudsa@naver.com>
2021-11-27 23:46:34 +0900

    2021-11-27-2

--- src/step2/LottoStore.java
+++ src/step2/LottoStore.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class LottoStore {
     public static void main(String[] args) {
         System.out.println("Hello Good day");
+        System.out.println("What can i do for you?");
         Scanner scan = new Scanner(System.in);
         int buyLottoGameCnt = scan.nextInt();
     }
